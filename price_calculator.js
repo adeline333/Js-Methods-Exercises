6a01758f223cb7bca24318e0159e6fa67edb92b8
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2024-11-30 21:26:27 +0200

    added the solution to the price calculator exercises but without proper calculation of the final price

--- price_calculator.js
+++ price_calculator.js
@@ -2,4 +2,23 @@
 
 */
 
-funtion priceCalculator(array){}
+function totalPrice(element){
+    let discounted= (element.price-(element.price*0.2));
+    let final=(0.1*element.price)+element.price;
+
+
+
+    return {name:element.name, price:element.price, discounted:discounted, final:final};
+}
+
+
+function priceCalculator(array){
+    let newPrice= array.map(totalPrice);
+    return newPrice;
+}
+
+console.log(  priceCalculator( [
+    { name: 'Laptop', price: 1000 },
+    { name: 'Phone', price: 500 },
+    { name: 'Tablet', price: 300 }
+  ]));
